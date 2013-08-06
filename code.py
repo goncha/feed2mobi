310966a533efac8f77f6ff5a40c3a56f00d89999
Gang Chen <goncha@gmail.com>
2013-08-06 23:35:32 +0800

    Replace google login with openid login

--- code.py
+++ code.py
@@ -6,6 +6,8 @@ import os
 
 import feed
 import web
+import web.webopenid
+
 
 # Chdir into this module file's directory
 moduledir = os.path.dirname(__file__)
@@ -26,104 +28,41 @@ app = web.auto_application()
 # Webpy database
 db = web.database(dbn='sqlite', db=os.path.join(datapath, 'feed2mobi.db'))
 
-# Webpy session
-if not web.config.get('_session'):
-    session = web.session.Session(app,
-                                  web.session.DiskStore(os.path.join(datapath, 'sessions')))
-    web.config._session = session
-else:
-    session = web.config._session
-
 # Webpy template
-render = web.template.render('templates/',
-                             base='layout',
-                             globals={'context': session})
+render = web.template.render(base='layout',
+                             globals={'openid': web.webopenid,
+                                      'context': web.ctx})
 
 # Feed manager
 mgr = feed.FeedManager(db, datapath=datapath)
 
 
 # ----------------------------------------
-# Google account service
+# OpenID service
 # ----------------------------------------
-from urllib2 import urlopen
-from urllib import urlencode
-import re
+app.add_mapping(r'/openid', 'web.webopenid.host')
 
-_GOOGLE_OPENID_PARAMS = {
-    'openid.ns': 'http://specs.openid.net/auth/2.0',
-    'openid.ns.pape': 'http://specs.openid.net/extensions/pape/1.0',
-    'openid.ns.max_auth_age': '0',
-    'openid.claimed_id': 'http://specs.openid.net/auth/2.0/identifier_select',
-    'openid.identity': 'http://specs.openid.net/auth/2.0/identifier_select',
-    # 'openid.return_to': 'http://www.example.com/checkauth',
-    # 'openid.realm': 'http://www.example.com/',
-    # 'openid.assoc_handle': 'ABSmpf6DNMw',
-    'openid.mode': 'checkid_setup',
-    'openid.ui.ns': 'http://specs.openid.net/extensions/ui/1.0',
-    'openid.ui.mode': 'popup',
-    'openid.ui.icon': 'true',
-    'openid.ns.ax': 'http://openid.net/srv/ax/1.0',
-    'openid.ax.mode': 'fetch_request',
-    'openid.ax.type.email': 'http://axschema.org/contact/email',
-    'openid.ax.required': 'email',
-    }
-_GOOGLE_OPENID_URL = 'https://www.google.com/accounts/o8/id'
-_XRDS_RE = re.compile(u'<URI>([^<]+)</URI>')
-def google_login():
-    params = dict(_GOOGLE_OPENID_PARAMS)
-    params['openid.return_to'] = web.ctx.home + web.http.url('/checkauth')
-    queryStr = '?'+urlencode(params)
-    try:
-        resp = urlopen(_GOOGLE_OPENID_URL + queryStr)
-        m = _XRDS_RE.search(resp.read().decode('utf-8'))
-        if m:
-            uri = m.group(1)
-            raise web.seeother(uri + queryStr)
-    except:
-        pass
-    return render.notlogin('Cannot connect Google Account service')
-
-def require_auth(fn):
+def set_auth_info(fn):
     def new_func(*args, **kws):
-        if not (session.get('account') and session.get('account_id')):
-            google_login()
-        else:
-            return fn(*args, **kws)
+        oid = web.webopenid.status()
+        if oid:
+            account_id, account_actived = mgr.account(oid)
+            web.ctx.account_id = account_id
+            web.ctx.account_actived = account_actived
+        return fn(*args, **kws)
     return new_func
 
 
-
-# ----------------------------------------
-# Auth web handlers
-# ----------------------------------------
-class login(app.page):
-    def GET(self):
-        if session.get('account'):
-            raise web.seeother(web.ctx.home + web.http.url('/'))
-        else:
-            google_login()
-
-
-class checkauth(app.page):
-    def GET(self):
-        i = web.input()
-        if i.get('openid.mode') == 'id_res':
-            account = i.get('openid.ext1.value.email')
-            account_id, account_actived = mgr.account(account)
-            if account_actived:
-                session['account'] = account
-                session['account_id'] = account_id
-                raise web.seeother(web.ctx.home + web.http.url('/'))
+def require_auth(fn):
+    def new_func(*args, **kws):
+        if web.ctx.get('account_id'):
+            if web.ctx.get('account_actived'):
+                return fn(*args, **kws)
             else:
                 raise web.forbidden('Account has been locked')
-        return render.notlogin('Unsuccessful log-in or has declined to approve authentication')
-
-
-class logout(app.page):
-    def GET(self):
-        session.kill()
-        raise web.seeother(web.ctx.home + web.http.url('/'))
+        else:
+            raise web.seeother(web.ctx.home + web.http.url('/'))
+    return new_func
 
 
 # ----------------------------------------
@@ -133,10 +72,11 @@ class top(app.page):
     path='^/(?:top)?(?:\?o=[0-9]+)?$'
     pagesize = 15
 
+    @set_auth_info
     def GET(self):
         i = web.input()
         offset = int(i.get('o', 0))
-        feeds = mgr.list(account=session.get('account_id'),
+        feeds = mgr.list(account=web.ctx.get('account_id'),
                          offset=offset, limit=self.pagesize+1)
         feeds, prevOffset, nextOffset = self.calcPageData(feeds, offset)
         return render.list(feeds, prevOffset, nextOffset, self.page())
@@ -153,10 +93,11 @@ class top(app.page):
 class new(top):
     path='^/new(?:\?o=[0-9]+)?$'
 
+    @set_auth_info
     def GET(self):
         i = web.input()
         offset = int(i.get('o', 0))
-        feeds = mgr.listNew(session.get('account_id'),
+        feeds = mgr.listNew(web.ctx.get('account_id'),
                             offset=offset, limit=self.pagesize+1)
         feeds, prevOffset, nextOffset = self.calcPageData(feeds, offset)
         return render.list(feeds, prevOffset, nextOffset, self.page())
@@ -165,13 +106,12 @@ class new(top):
 class subscribed(top):
     path='^/subscribed(?:\?o=[0-9]+)?$'
 
+    @set_auth_info
     @require_auth
     def GET(self):
-        if not session.get('account'):
-            raise web.forbidden('Not logged in')
         i = web.input()
         offset = int(i.get('o', 0))
-        feeds = mgr.listSubscribed(session['account_id'],
+        feeds = mgr.listSubscribed(web.ctx.get('account_id'),
                                    offset=offset, limit=self.pagesize+1)
         feeds, prevOffset, nextOffset = self.calcPageData(feeds, offset)
         return render.list(feeds, prevOffset, nextOffset, self.page())
@@ -180,41 +120,46 @@ class subscribed(top):
 class subscribe(app.page):
     path='^/subscribe/([0-9]+)$'
 
+    @set_auth_info
     @require_auth
     def GET(self, feed):
-        mgr.subscribe(feed, session['account_id'])
+        mgr.subscribe(feed, web.ctx.get('account_id'))
         raise web.seeother(web.ctx.home + web.http.url('/subscribed'))
 
 
 class subscribe1(app.page):
     path='^/subscribe$'
 
+    @set_auth_info
     @require_auth
     def POST(self):
         i = web.input()
-        mgr.subscribe(i.feed, session['account_id'])
+        mgr.subscribe(i.feed, web.ctx.get('account_id'))
         raise web.seeother(web.ctx.home + web.http.url('/subscribed'))
 
 
 class unsubscribe(app.page):
     path='^/unsubscribe/([0-9]+)$'
 
+    @set_auth_info
     @require_auth
     def GET(self, feed):
-        mgr.unsubscribe(feed, session['account_id'])
+        mgr.unsubscribe(feed, web.ctx.get('account_id'))
         raise web.seeother(web.ctx.home + web.http.url('/subscribed'))
 
 
 class delivery(app.page):
 
+    @set_auth_info
     @require_auth
     def GET(self):
         delivery = list(db.select(['account'],
                                   what='delivery_hour hour,delivery_address address,delivery_actived actived,delivery_bundle bundle',
                                   where='id=$account',
-                                  vars={'account': session['account_id']}))
+                                  vars={'account': web.ctx.get('account_id')}))
         return render.delivery(delivery[0])
 
+    @set_auth_info
     @require_auth
     def POST(self):
         i = web.input()
@@ -231,7 +176,7 @@ class delivery(app.page):
                   delivery_hour = hour,
                   delivery_address = address,
                   delivery_bundle = bundle,
-                  vars={'account': session['account_id']})
+                  vars={'account': web.ctx.get('account_id')})
         raise web.seeother(web.ctx.home + web.http.url('/delivery'))
 
 
